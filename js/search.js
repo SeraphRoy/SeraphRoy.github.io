a7dae3c736fcd1ef45edc489d0c3bf064e4f1d2b
SeraphRoy <royxagain@gmail.com>
2018-05-04 13:33:28 -0700

    adjust search bar

--- js/search.js
+++ js/search.js
@@ -6,7 +6,7 @@ jQuery(function() {
             }
         }
         var searchbar_width = $("#site_search").width();
-        $("#search_box").width(searchbar_width - 65);
+        $("#search_box").width(searchbar_width - 15);
     }
 
     adjust_search_box_width();
